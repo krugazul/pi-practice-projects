b91263d324187df371ba011703a1c8f4b5979e31
Warwick Booth <warwick@lsdev.biz>
2019-11-30 19:04:23 +0200

    Added in the letters a to f of the 7 segment display

---
+++ gpio/7-segment-display/7seg-test.py
@@ -0,0 +1,6 @@
+import sega
+import segb
+import segc
+import segd
+import sege
+import segf
---
+++ gpio/7-segment-display/segf.py
@@ -0,0 +1,31 @@
+from gpiozero import DigitalOutputDevice
+from time import sleep
+
+pinAred = DigitalOutputDevice(17)
+pinByellow = DigitalOutputDevice(27)
+pinCgreen = DigitalOutputDevice(22)
+pinDblack = DigitalOutputDevice(23)
+
+pinEred = DigitalOutputDevice(13)
+pinFyellow = DigitalOutputDevice(6)
+pinGgreen = DigitalOutputDevice(5)
+pinHblack = DigitalOutputDevice(12)
+
+pinAred.off()
+pinByellow.on()
+pinCgreen.on()
+pinDblack.on()
+pinEred.off()
+pinFyellow.off()
+pinGgreen.off()
+pinHblack.on()
+sleep(1)
+
+pinAred.close()
+pinByellow.close()
+pinCgreen.close()
+pinDblack.close()
+pinEred.close()
+pinFyellow.close()
+pinGgreen.close()
+pinHblack.close()
