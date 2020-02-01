d7d20c2f6d8f7401181930ea139e342528ae4105
Warwick Booth <warwick@lsdev.biz>
2020-02-02 00:54:15 +0200

    Adding in an LED and BUTTON script.

---
+++ gpio/led-2 (3 leg)/led_test.py
@@ -0,0 +1,15 @@
+from gpiozero import LED
+from time import sleep
+
+led_1 = LED(21)
+led_2 = LED(6)
+
+while True:
+    led_1.on()
+    sleep(1)
+    led_1.off()
+    sleep(1)
+    led_2.on()
+    sleep(1)
+    led_2.off()
+    sleep(1)
